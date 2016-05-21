cae57df54716c54dc4d6dffd3581b7526accb6fd
filosoff26 <filosoff26@gmail.com>
2016-05-21 11:38:19 +0000

    Update mysql.php

--- mysql.php
+++ mysql.php
@@ -6,9 +6,9 @@ ini_set('display_erors', TRUE);
 ini_set('display_startup_erors', TRUE);
 
 $mysql_host = 'localhost';
-$mysql_user = 'floran3com_soft';
-$mysql_password = 'nadip8913';
-$mysql_db_name = 'floran3com_soft';
+$mysql_user = '';
+$mysql_password = '';
+$mysql_db_name = '';
 $mysql_prefix = 'nadip_';
 mysql_connect($mysql_host, $mysql_user, $mysql_password);
 mysql_select_db($mysql_db_name);
